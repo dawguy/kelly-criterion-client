c41a38aa0d8cb7f68fef55ebcaf774dfae8ebe45
David Wright <hockeyguy411@gmail.com>
2022-06-02 12:27:15 -0400

    Respect Gitignore for auto-updated files

--- public/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { kelly_criterion_client.browser.init(); } catch (e) { console.error("An error occurred when calling (kelly-criterion-client.browser/init)"); throw(e); }
