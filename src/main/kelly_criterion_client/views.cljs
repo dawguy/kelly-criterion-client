40710bbce961423cd7448167304f31c56ea6987e
David Wright <hockeyguy411@gmail.com>
2022-06-03 08:55:27 -0400

    Adds insights container which provides exact values for a given probability distribution.

--- src/main/kelly_criterion_client/views.cljs
+++ src/main/kelly_criterion_client/views.cljs
@@ -16,8 +16,8 @@
 
 (defn chanceListContainer [chance]
   [:tr {:key (random-uuid)}
-   [:td.w-full.p-3.text-center.border.lg:table-cell.border-b (:prob chance)]
-   [:td.w-full.p-3.text-center.border.lg:table-cell.border-b (:payout chance)]
+   [:td.p-3.text-center.border.lg:table-cell.border-b {:class "w-1/2"} (:prob chance)]
+   [:td.p-3.text-center.border.lg:table-cell.border-b {:class "w-1/2"} (:payout chance)]
    ])
 
 (defn chancesContainer [c]
@@ -31,6 +31,32 @@
       [:tbody
        (map chanceListContainer c)
        ]]]])
+
+(def chances [{:prob 0.15 :payout 300} {:prob 0.35 :payout 200} {:prob 0.5 :payout 0}])
+
+(defn insightsListContainer [label value]
+  [:tr {:key (random-uuid)}
+   [:td.w-full.p-2.lg:table-cell {:class "w-1/2"} label]
+   [:td.w-full.p-2.lg:table-cell {:class "w-1/2"} value]
+   ])
+
+(defn insightsContainer [chances]
+  (let [expected-value (/ (reduce #(+ %1 (* (:payout %2) (:prob %2))) 0 chances) (* 100 (reduce #(+ %1 (:prob %2)) 0 chances)))
+        optimal-growth-rate (kelly/optimal-growth-rate chances)]
+    [:div.relative.px-4.py-4.flex-1.mx-1.my-1.bg-gray-100.border-2.border-gray-300.border-solid.rounded
+     [:h3.text-lg.-mb-4.mr-4 "Insights"]
+     [:div.mt-7.block.w-full.overflow-x-auto
+      [:table.w-full.whitespace-nowrap.border-none.border-0
+       [:thead]
+       [:tbody.text-left.border-collapse
+        (insightsListContainer "Expected Value" expected-value)
+        (insightsListContainer "Optimal Growth Rate Percentage" (first optimal-growth-rate))
+        (insightsListContainer "Optimal Growth Rate Value" (second optimal-growth-rate))
+        (insightsListContainer "Expected To Beat Market?" (cond
+                                                            (> 0 (second optimal-growth-rate)) "No"
+                                                            (< expected-value 1.10) "No"
+                                                            :else "Yes"))
+      ]]]]))
 
 (defn growthRateContainer [growth-rates]
   ;; Graph
@@ -51,8 +77,8 @@
     [:div (chancesContainer unnormalized-chances)]
     [:div.flex.flex-wrap.justify-center.align-center
      [:div.flex-initial.sm:max-w-96.sm:max-h-96 {:class "w-full sm:w-1/2"} (chancesGraphContainer chances)]
-     [:div.flex-initial.sm:max-w-96.sm:max-h-96 {:class "w-full sm:w-1/2"} (growthRateContainer (kelly/find-growth-rates chances))]
-     ]
+     [:div.flex-initial.sm:max-w-96.sm:max-h-96 {:class "w-full sm:w-1/2"} (growthRateContainer (kelly/find-growth-rates chances))]]
+    [:div (insightsContainer chances)]
     ]])
 
 (defn page [data]
